14d614332f660d90ddfc3a0038322c691a72b5b0
woshiwwy16@126.com <Wwy-+901116>
2020-05-09 16:51:51 +0800

    modify image view binding adapter

--- kbviewlib/src/main/java/com/kbit/kbviewlib/binding/ImageViewBindingAdapter.java
+++ kbviewlib/src/main/java/com/kbit/kbviewlib/binding/ImageViewBindingAdapter.java
@@ -8,7 +8,7 @@ import androidx.databinding.BindingAdapter;
 import com.bumptech.glide.Glide;
 
 public class ImageViewBindingAdapter {
-    @BindingAdapter(value = {"android:image_url", "android:place_holder", "android:error_image"}, requireAll = false)
+    @BindingAdapter(value = {"app:image_url", "app:place_holder", "app:error_image"}, requireAll = false)
     public static void loadImage(ImageView imageView, String imageUrl, Drawable placeHolder, Drawable error) {
         Glide.with(imageView).load(imageUrl).placeholder(placeHolder).error(error).into(imageView);
     }
