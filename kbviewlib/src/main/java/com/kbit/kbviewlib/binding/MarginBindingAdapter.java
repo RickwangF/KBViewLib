38952859c11a6a06628ef9cd46da83541bf917c1
woshiwwy16@126.com <Wwy-+901116>
2020-05-12 16:06:29 +0800

    modify margin binding adapter

--- kbviewlib/src/main/java/com/kbit/kbviewlib/binding/MarginBindingAdapter.java
+++ kbviewlib/src/main/java/com/kbit/kbviewlib/binding/MarginBindingAdapter.java
@@ -5,12 +5,18 @@ import android.view.ViewGroup;
 
 import androidx.databinding.BindingAdapter;
 
+import com.kbit.kbbaselib.util.DeviceUtil;
+import com.kbit.kbbaselib.util.DisplayUtil;
+
 public class MarginBindingAdapter {
 
     @BindingAdapter(value = {"marginLeft", "marginTop", "marginRight", "marginBottom"}, requireAll = false)
     public static void setMargin(View view, float left, float top, float right, float bottom) {
         ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
-        layoutParams.setMargins(Math.round(left), Math.round(top), Math.round(right), Math.round(bottom));
+        layoutParams.setMargins(Math.round(left * DisplayUtil.getDensity()),
+                Math.round(top * DisplayUtil.getDensity()),
+                Math.round(right * DisplayUtil.getDensity()),
+                Math.round(bottom * DisplayUtil.getDensity()));
         view.setLayoutParams(layoutParams);
     }
 }
