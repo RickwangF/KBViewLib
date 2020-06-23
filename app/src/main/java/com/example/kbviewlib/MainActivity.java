feeb50d1d0754c4a8a0faaf8ca68d701951540fc
woshiwwy16@126.com <Wwy-+901116>
2020-06-23 15:08:14 +0800

    modify image view binding adapter

--- app/src/main/java/com/example/kbviewlib/MainActivity.java
+++ app/src/main/java/com/example/kbviewlib/MainActivity.java
@@ -33,30 +33,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        long timestamp = DateUtil.getNowTimeStamp(true);
-        Log.e("date", "timestamp is " + timestamp);
-
-        List<TestModel> modelList = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            TestModel model = new TestModel();
-            model.setTitle("这是一条新闻的标题，这是一条标题" + i);
-            model.setImageUrl("http://kmzsccfile.kmzscc.com/Uploads/image/2018/08/20/5b7a96b76aabe.jpg");
-            model.setTime(1588855251);
-            modelList.add(model);
-        }
-
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        TestAdapter adapter = new TestAdapter(this, modelList);
-        mBind.rvMain.setLayoutManager(layoutManager);
-        mBind.rvMain.setAdapter(adapter);
-        mBind.rvMain.setHasFixedSize(true);
+        TestModel model = new TestModel();
+        model.setImageUrl("https://dingyue.ws.126.net/2020/0623/04b53c08j00qccvia000lc000hs009nm.jpg");
+        mBind.setModel(model);
+//        long timestamp = DateUtil.getNowTimeStamp(true);
+//        Log.e("date", "timestamp is " + timestamp);
+//
+//        List<TestModel> modelList = new ArrayList<>();
+//        for (int i = 0; i < 20; i++) {
+//            TestModel model = new TestModel();
+//            model.setTitle("这是一条新闻的标题，这是一条标题" + i);
+//            model.setImageUrl("http://kmzsccfile.kmzscc.com/Uploads/image/2018/08/20/5b7a96b76aabe.jpg");
+//            model.setTime(1588855251);
+//            modelList.add(model);
+//        }
+//
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+//        TestAdapter adapter = new TestAdapter(this, modelList);
+//        mBind.rvMain.setLayoutManager(layoutManager);
+//        mBind.rvMain.setAdapter(adapter);
+//        mBind.rvMain.setHasFixedSize(true);
     }
 
     private void initHeadView() {
-        BigImageModel bigImageModel = new BigImageModel();
-        bigImageModel.setImageUrl("https://rmt-chenggong-file.vcdn.kmzscc.com/cg/2020/04/5e893000d85b9/2yaYmkri7W.jpg");
-        headBind = DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.header_test, mBind.rvMain, false);
-        headBind.setModel(bigImageModel);
-        mBind.rvMain.addHeaderView(headBind.getRoot());
+//        BigImageModel bigImageModel = new BigImageModel();
+//        bigImageModel.setImageUrl("https://rmt-chenggong-file.vcdn.kmzscc.com/cg/2020/04/5e893000d85b9/2yaYmkri7W.jpg");
+//        headBind = DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.header_test, mBind.rvMain, false);
+//        headBind.setModel(bigImageModel);
+//        mBind.rvMain.addHeaderView(headBind.getRoot());
     }
 }
--- kbviewlib/src/main/java/com/kbit/kbviewlib/binding/ImageViewBindingAdapter.java
+++ kbviewlib/src/main/java/com/kbit/kbviewlib/binding/ImageViewBindingAdapter.java
@@ -1,19 +1,33 @@
 package com.kbit.kbviewlib.binding;
 
+import android.annotation.SuppressLint;
 import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
 
 import androidx.databinding.BindingAdapter;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.kbit.kbbaselib.util.StringUtil;
 
 public class ImageViewBindingAdapter {
-    @BindingAdapter(value = {"app:image_url", "app:image_res", "app:place_holder", "app:error_image"}, requireAll = false)
-    public static void loadImage(ImageView imageView, String imageUrl, Drawable resource, Drawable placeHolder, Drawable error) {
-        if (imageUrl != null && !imageUrl.isEmpty()) {
-            Glide.with(imageView).load(imageUrl).placeholder(placeHolder).error(error).into(imageView);
+    @BindingAdapter(value = {"app:image_url", "app:image_res", "app:place_holder", "app:error_image", "app:is_round"}, requireAll = false)
+    public static void loadImage(ImageView imageView, String imageUrl, Drawable resource, Drawable placeHolder, Drawable error, boolean isRound) {
+        RequestOptions requestOptions = new RequestOptions();
+        if (placeHolder != null) {
+            requestOptions = requestOptions.placeholder(placeHolder);
+        }
+        if (error != null) {
+            requestOptions = requestOptions.error(error);
+        }
+        if (isRound) {
+            requestOptions = requestOptions.circleCrop();
+        }
+
+        if (!StringUtil.isEmpty(imageUrl)) {
+            Glide.with(imageView).load(imageUrl).apply(requestOptions).into(imageView);
         } else if (resource != null) {
-            Glide.with(imageView).load(resource).placeholder(placeHolder).error(error).into(imageView);
+            Glide.with(imageView).load(resource).apply(requestOptions).into(imageView);
         }
     }
 }
